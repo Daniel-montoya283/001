89becab070c7578232057312ead55efbe3175da8
Daniel Montoya <montoyad283@gmail.com>
2022-10-23 11:27:35 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+
+//barra lateral
+const btnToggle = document.querySelector('.toggle-btn') 
+
+btnToggle.addEventListener('click', function () {
+    document.getElementById('sidebar').classList.toggle('active');
+})
+
+//modal
+
+const openModal = document.querySelector('.imagenPerfil');
+const modal = document.querySelector('.modal')
+const closeModal = document.querySelector('.modal__close')
+
+openModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.classList.add('modal--show')
+});
+
+closeModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.classList.remove('modal--show')
+})
+
+//cerrar
+
+const cerrar = document.querySelector('#cerrar')
+
+cerrar.addEventListener('click', () => {
+
+    const site = 'http://127.0.0.1:5500/';
+
+    window.close(site);
+})
